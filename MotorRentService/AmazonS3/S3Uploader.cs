8b174c01118effba28a741ef09762cc027bd9d98
gabriel aparecido mizael <gabri@NITROV15>
2025-08-31 18:38:28 -0300

    `Amazon S3` method for `storing driver's license image`, validate `CNH image and CNPJ` method created and logging and error handling added.

---
+++ MotorRentService/AmazonS3/S3Uploader.cs
@@ -0,0 +1,47 @@
+﻿#region Mainetence
+/*
+Comment: Created class.
+Created: 08/31/2024 18:31
+Author: Gabriel MS
+*/
+#endregion
+
+using Amazon.CDK;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+using System.IO;
+
+public class S3Uploader
+{
+    private string bucketName = "motorrentbucket";
+    private IAmazonS3 s3Client;
+
+    public S3Uploader()
+    {
+        // Configuração do cliente (as credenciais devem ser configuradas)
+        s3Client = new AmazonS3Client(Amazon.RegionEndpoint.USEast2);
+        
+    }
+
+    public void UploadFoto(string caminhoArquivoOrigem, string nomeArquivo)
+    {
+        try
+        {
+            var fileTransferUtility = new TransferUtility(s3Client);
+            var fileInfo = new FileInfo(caminhoArquivoOrigem);
+
+            fileTransferUtility.Upload(caminhoArquivoOrigem, bucketName, nomeArquivo);
+
+            Console.WriteLine($"Foto '{nomeArquivo}' enviada com sucesso para o bucket '{bucketName}'.");
+        }
+        catch (AmazonS3Exception e)
+        {
+            Console.WriteLine($"Erro ao fazer upload da foto: {e.Message}");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"Erro inesperado: {e.Message}");
+        }
+    }
+}
--- MotorRentService/Controllers/DeliveryPersonController.cs
+++ MotorRentService/Controllers/DeliveryPersonController.cs
@@ -3,64 +3,173 @@
 Comment: Created Mainetence Region and correction mapping.
 Created: 08/31/2024 15:00
 Author: Gabriel MS
+----------------------------------------------------------------------
+Comment: Validate CNH image and CNPJ method created, logging and error 
+         handling added, and repository method for updating CNH image.
+Created: 08/31/2024 18:31
+Author: Gabriel MS
 */
 #endregion
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage;
 using MotorRentService.Data;
 using MotorRentService.Dtos;
 using MotorRentService.Models;
 using MotorRentService.RabbitMqClient;
+using System.Linq.Expressions;
+using System.Text.Json;
 
 namespace MotorRentService.Controllers;
 
-[Route("api/[controller]")]
+[Route("entregadores/")]
 [ApiController]
+[Produces("application/json")]
 public class DeliveryPersonController : Controller
 {
     private readonly IDeliveryPersonRepository _repository;
     private readonly IMapper _mapper;
+    private readonly ILogger<DeliveryPersonController> _logger;
 
-
-    public DeliveryPersonController(IDeliveryPersonRepository repository, IMapper mapper)
+    public DeliveryPersonController(IDeliveryPersonRepository repository, IMapper mapper, ILogger<DeliveryPersonController> logger)
     {
         _repository = repository;
         _mapper = mapper;
+        _logger = logger;
     }
 
-    [HttpPost("CreateDeliveryPerson")]
+
+    /// <summary>
+    /// Create a Delivery Person
+    /// </summary>
+    /// <param name="Motorcycle">Motorcycle creation data</param>
+    /// <returns>Created motorcycle</returns>
+    [HttpPost()]
+    [ProducesResponseType(typeof(MotorcycleDto), StatusCodes.Status200OK)]
+    [ProducesResponseType(400)]
     public async Task<ActionResult<DeliveryPersonDto>> CreateDeliveryPerson(DeliveryPersonCreateDto DeliveryPersonCreateDto)
     {
-        var deliveryPerson = _mapper.Map<DeliveryPerson>(DeliveryPersonCreateDto);
-        _repository.CreateDeliveryPerson(deliveryPerson);
+        try
+        {
+            _logger.LogInformation("START - Creating a new delivery person with CNPJ: {CNPJ}", DeliveryPersonCreateDto.CNPJ);
+            var deliveryPerson = _mapper.Map<DeliveryPerson>(DeliveryPersonCreateDto);
+            _repository.CreateDeliveryPerson(deliveryPerson);
 
-        var deliveryPersonDto = _mapper.Map<DeliveryPersonDto>(deliveryPerson);
+            var deliveryPersonDto = _mapper.Map<DeliveryPersonDto>(deliveryPerson);
 
-        return Ok(deliveryPersonDto);
+            _logger.LogInformation("END - Created a new delivery person with CNPJ: {CNPJ}", DeliveryPersonCreateDto.CNPJ);
+
+            return Ok(deliveryPersonDto);
+        }
+        catch (Exception ex)
+        {
+            Error response = new Error
+            {
+                Message = "Dados inválidos",
+            };
+            _logger.LogError("ERROR - Creating a new delivery person with CNPJ: {CNPJ} - {ErrorMessage}", DeliveryPersonCreateDto.CNPJ, ex.Message);
+
+            return Json(response);
+        }
     }
 
     [HttpGet("{cnpj}", Name = "GetDeliveryPersonByCNPJNumber")]
     public ActionResult<DeliveryPersonDto> GetDeliveryPersonByCNPJNumber(string cnpj)
     {
-        var deliveryPerson = _repository.GetDeliveryPersonByCNPJNumber(cnpj);
-        if (deliveryPerson != null)
+        try
         {
-            return Ok(_mapper.Map<DeliveryPersonDto>(deliveryPerson));
-        }
-        return NotFound();
-    }
-
-    [HttpPost("UpdateCNHImage/{cnpj}")]
-    public ActionResult UpdateCNHImage(string cnpj, [FromBody] string cnhImagePath)
-    {
-        var deliveryPerson = _repository.GetDeliveryPersonByCNPJNumber(cnpj);
-        if (deliveryPerson == null)
-        {
+            _logger.LogError("START - Getting delivery person by CNPJ");
+            var deliveryPerson = _repository.GetDeliveryPersonByCNPJNumber(cnpj);
+            if (deliveryPerson != null)
+            {
+                _logger.LogError("END - Getting delivery person by CNPJ");
+                return Ok(_mapper.Map<DeliveryPersonDto>(deliveryPerson));
+            }
+            _logger.LogError("END - Getting delivery person by CNPJ");
             return NotFound();
         }
-        deliveryPerson.CNHImage = cnhImagePath;
-        _repository.UpdateCNHImage(deliveryPerson);
-        return NoContent();
+        catch (Exception ex)
+        {
+            Error response = new Error
+            {
+                Message = ex.Message,
+            };
+            _logger.LogError("ERROR - Getting delivery person by CNPJ: {cnpj} - {ErrorMessage}", cnpj, ex.Message);
+
+            return Json(response);
+        }
     }
+
+    /// <summary>
+    /// Send CNH image to S3 and update delivery person
+    /// </summary>
+    /// <param name="Motorcycle">Motorcycle creation data</param>
+    /// <returns>Created motorcycle</returns>
+    [HttpPost("{cnpj}/cnh")]
+    [ProducesResponseType(typeof(MotorcycleDto), StatusCodes.Status201Created)]
+    [ProducesResponseType(400)]
+    []
+    public ActionResult UpdateCNHImage(string cnpj, string cnhImagePath)
+    {
+        try
+        {
+            _logger.LogError("START - Updating CNH image for delivery person by CNPJ");
+            _logger.LogError("START - Validating CNH image and CNPJ");
+
+            bool validate = ValidatePictureCnpj(cnhImagePath, cnpj);
+            var deliveryPerson = _repository.GetDeliveryPersonByCNPJNumber(cnpj);
+            if (deliveryPerson == null || !validate)
+            {
+                _logger.LogError("END - Validating CNH image and CNPJ");
+
+                return NotFound();
+            }
+            _logger.LogError("END - Validating CNH image and CNPJ");
+
+            deliveryPerson.CNHImage = cnhImagePath;
+            S3Uploader s3Uploader = new S3Uploader();
+            s3Uploader.UploadFoto(cnhImagePath, cnpj);
+            _repository.UpdateCNHImage(deliveryPerson);
+            return NoContent();
+        }
+        catch (Exception ex)
+        {
+
+            Error response = new Error
+            {
+                Message = "Dados inválidos",
+            };
+            _logger.LogError("ERROR - Getting delivery person by CNPJ: {cnpj} - {ErrorMessage}", cnpj, ex.Message);
+
+            return Json(response);
+        }
+    }
+
+    private bool ValidatePictureCnpj(string cnhImagePath, string cnpj)
+    {
+        if (string.IsNullOrEmpty(cnhImagePath) || string.IsNullOrEmpty(cnpj))
+        {
+            _logger.LogError("END - Validating CNH image and CNPJ");
+            return false;
+        }
+
+        if (!System.IO.File.Exists(cnhImagePath))
+        {
+            _logger.LogError("END - Validating CNH image and CNPJ");
+            return false;
+        }
+
+        string[] extensionsPermit = [".png", ".bmp"];
+        string extension = Path.GetExtension(cnhImagePath).ToLower();
+
+        if (!extensionsPermit.Contains(extension))
+        {
+            _logger.LogError("END - Validating CNH image and CNPJ");
+            return false;
+        }
+
+        return true;
+    }
+
 }
 
